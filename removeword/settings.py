986e35c8121ca8e922a142efdb086d4f61c6e76c
YAKSHBHUT <106272637+YAKSHBHUT@users.noreply.github.com>
2022-05-28 19:28:49 +0530

    Update settings.py

--- removeword/settings.py
+++ removeword/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-omtukl850)+-*9d#8b8mn_7w3&2(#iycvdkje2#-1il%^ql354'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
